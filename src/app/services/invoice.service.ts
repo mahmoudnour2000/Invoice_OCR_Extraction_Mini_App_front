db75f04878d6ee6aac1a0419214a151af254e6f4
Mahmoud Nour <90916796+mahmoudnour2000@users.noreply.github.com>
2025-07-19 03:28:06 +0300

    Updated invoice.service.ts

--- src/app/services/invoice.service.ts
+++ src/app/services/invoice.service.ts
@@ -8,7 +8,7 @@ import { Invoice, InvoiceDetails, UploadResponse, ApiResponse } from '../models/
   providedIn: 'root'
 })
 export class InvoiceService {
-  private baseUrl = 'http://localhost:5001/api';
+  private baseUrl = 'https://localhost:5001/api';
   
   private httpOptions = {
     headers: new HttpHeaders({
